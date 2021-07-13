24040e65f063eef27e818d7a8367c9b1fdec1448
Una <trewbot@gmail.com>
2021-07-13 01:46:25 -0700

    feat(view): add about view

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
   return HttpResponse('<h1>Hello, World!</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About WaifuCollector</h1>')
