1d61415cebb1ef46621c304211b00cc625ac4374
Una <trewbot@gmail.com>
2021-07-13 01:49:31 -0700

    feat(view): add template for about view

--- main/views.py
+++ main/views.py
@@ -5,4 +5,4 @@ def home(request):
   return HttpResponse('<h1>Hello, World!</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About WaifuCollector</h1>')
+  return render(request, 'about.html')
